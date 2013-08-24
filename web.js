dca581edd6471823fb8585b8a0ae4af2dc696399
Yinka Osoba <yosoba@hotmail.com>
2013-08-24 21:46:22 +0000

    updated web.js

--- web.js
+++ web.js
@@ -1,19 +1,12 @@
-//#!/usr/bin/env node
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
 var fs = require('fs');
-//var data = fs.readFileSync('./index.html', 'utf8');
-// wait for the result, then use it
-//console.log(data);
-
 var buffer = fs.readFileSync('./index.html', 'utf8');
-//console.log(buffer.toString("utf-8"));
 
 app.get('/', function(request, response) {
   response.send(buffer.toString("utf-8"));
-//response.send('hello');
 });
 
 var port = process.env.PORT || 5000;
